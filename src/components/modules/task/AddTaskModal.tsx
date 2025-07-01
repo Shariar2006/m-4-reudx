35a911ab4c008293bd9c0b23ca74858ec805ce67
Shariar2006 <shariarnafis86@gmail.com>
2025-07-01 21:59:55 +0600

    add task using redux

--- src/components/modules/task/AddTaskModal.tsx
+++ src/components/modules/task/AddTaskModal.tsx
@@ -16,6 +16,8 @@ import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { Textarea } from "@/components/ui/textarea"
 import { cn } from "@/lib/utils"
+import { addTask } from "@/redux/features/task/taskSlice"
+import { useAppDispatch } from "@/redux/hook"
 import { format } from "date-fns"
 import { CalendarIcon } from "lucide-react"
 import { useForm } from "react-hook-form"
@@ -23,9 +25,11 @@ import { useForm } from "react-hook-form"
 export function AddTaskModal() {
 
     const form = useForm()
+    const dispatch = useAppDispatch()
 
     const onSubmit = (data) => {
         console.log(data)
+        dispatch(addTask(data))
     }
 
     return (
--- src/redux/features/task/taskSlice.ts
+++ src/redux/features/task/taskSlice.ts
@@ -1,32 +1,38 @@
 import type { RootState } from "@/redux/store";
 import type { ITask } from "@/types";
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import { v4 as uuidv4 } from 'uuid';
 
 interface IInitialState {
     tasks: ITask[]
 }
 
 const initialState: IInitialState = {
-tasks:[
-    {
-        id: "dfkjdkj",
-        title: "initialize redux",
-        description: "create home page and routing",
-        dueDate: "2025-11",
-        isCompleted: false,
-        priority: "High"
-    },
-]
+    tasks: []
 }
 
 const taskSlice = createSlice({
     name: 'task',
     initialState,
-    reducers:{}
+    reducers: {
+        addTask: (state, action: PayloadAction<ITask>) => {
+            const id = uuidv4();
+
+            const taskData = {
+                ...action.payload,
+                id,
+                isCompleted: false
+            }
+
+            state.tasks.push(taskData)
+        }
+    }
 })
 
-export const tasks = (state: RootState)=>{
+export const tasks = (state: RootState) => {
     return state.todos.tasks
 }
+
+export const { addTask } = taskSlice.actions;
 
 export default taskSlice.reducer
