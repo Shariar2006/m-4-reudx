db61c4e6ab457451c5fa922a237a9cf0410e7fec
Shariar2006 <shariarnafis86@gmail.com>
2025-07-01 08:58:33 +0600

    add task modal and add 2 field

--- src/pages/Task.tsx
+++ src/pages/Task.tsx
@@ -1,3 +1,4 @@
+import { AddTaskModal } from "@/components/modules/task/AddTaskModal";
 import TaskCard from "@/components/modules/task/TaskCard";
 import { tasks } from "@/redux/features/task/taskSlice";
 import { useAppSelector } from "@/redux/hook";
@@ -8,12 +9,18 @@ const Task = () => {
     console.log(tasksData)
 
     return (
-        <div className="max-w-4xl mx-auto">
+        <div className="max-w-4xl mx-auto space-y-5">
+            <div className="flex justify-between items-center">
+                <h4>Tasks</h4>
+                <AddTaskModal></AddTaskModal>
+            </div>
+            <div>
             {
                 tasksData.map((task)=>(
                     <TaskCard key={task.id} task={task}/>
                 ))
             }
+            </div>
         </div>
     );
 };
