1fab9f6053b6da82117f4a965575bd52584a1d4b
Shariar2006 <shariarnafis86@gmail.com>
2025-07-03 08:19:57 +0600

    delete user

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,11 +1,25 @@
 import { AddUserModal } from "@/components/modules/user/AddTaskModal";
+import UserCard from "@/components/modules/user/UserCard";
+import { users } from "@/redux/features/user/userSlice";
+import { useAppSelector } from "@/redux/hook";
 
 const User = () => {
+    const userData = useAppSelector(users);
+
     return (
         <div className="max-w-4xl mx-auto space-y-5">
             <div className="flex justify-between items-center">
                 <h4>Users</h4>
                 <AddUserModal></AddUserModal>
+            </div>
+            <div>
+            <div>
+                {
+                    userData.map((user) => (
+                        <UserCard key={user.id} user={user} />
+                    ))
+                }
+            </div>
             </div>
         </div>
     );
